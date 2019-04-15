0ca52de06b10c524ac0faa3afb77b817163ee4c2
Vincent0530 <vincent.prado@upr.edu>
2019-04-15 17:01:34 -0400

    Tokens are now store in array split and cout contains the number of tokens.

--- HybridPL/main.c
+++ HybridPL/main.c
@@ -3,16 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
-
 #define lineSize 512
+
 
 int main() {
 	#pragma warning(disable : 4996)
-	const char* word;
+	const char* word;//
 	char buffer[lineSize];
 	const char* delims = " ";
-
-	printf("Example:\nVectorSum(1 2 3 4 5 9 7 3 6 8)\nPiCalculation(10)\n\n");
+	char **split = (char**) malloc(50 * sizeof(char*));
+	int i = 0;
+	int count = 0;
+	printf("example:\nvectorsum(1 2 3 4 5 9 7 3 6 8)\npicalculation(10)\n\n");
 	gets(buffer, lineSize);
 
 	printf("%s\n", buffer);
@@ -20,11 +22,22 @@ int main() {
 	word = strtok(buffer, delims);
 
 	while (word != NULL) {
-		printf("\nword: %s\n", word);
+		//printf("\nword: %s\n", word);
+		split[i] = word;
 		word = strtok(NULL, delims);
+		i++;
+		count++;
 	}
-
-	printf("\n%s\n", buffer);
+	i++;
+	split[i] = '\0';
+	i = 0;
+	while (i<count) {
+		printf("\n%s\n", split[i++]);			
+	}
+	free(split);
+	printf("count = %d", count);
+	
+	
 	getchar();
 	return 0;
 }
