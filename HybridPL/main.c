cfdcfa082be93b1bbe671fe010a9b13976f19639
Vincent0530 <vincent.prado@upr.edu>
2019-04-16 22:33:51 -0400

    Aded some rules to the language. List of string numbers are change to integers.

--- HybridPL/main.c
+++ HybridPL/main.c
@@ -7,38 +7,73 @@
 
 
 int main() {
-	#pragma warning(disable : 4996)
-	const char* word;//
-	char buffer[lineSize];
-	const char* delims = " ";
-	char **split = (char**) malloc(50 * sizeof(char*));
-	int i = 0;
-	int count = 0;
-	printf("example:\nvectorsum(1 2 3 4 5 9 7 3 6 8)\npicalculation(10)\n\n");
-	gets(buffer, lineSize);
+	#pragma warning(disable : 4996)//No borren esto.
+	/*---------------------------------------------Lexer-----------------------------------------------------*/
+	char buffer[lineSize];//En este buffer es que se escribe el input.
+	const char* word;//Cada Token de se guarda aqui y despues se pasa al string de arrays "split" que esta mas abajo.
+	const char* delims = " ,()";//Estod son los delimitadores que tengo por ahora.
+	char **split = (char**) malloc(50 * sizeof(char*));//Esto es lo mismo que un array de strings.
+	int i = 0;//Esta variable es para iterar.
+	int countTokens = 0;//Esta variable cuenta el numero de tokens que se encuentran en "split".
+	const char * tokens[] = { "VECTORSUM","PIAPROX","RP","LP","COMMA" };//Esto no lo he usado para nada.
+	printf("Example:\nVectorSum(1 2 3 4 5 9 7 3 6 8)\npicalculation(10)\n\n");//Esto es un ejemplo del lenguage.
 
-	printf("%s\n", buffer);
+	gets(buffer, lineSize);//Esto lo que hace es que pide un string y lo guarda en el buffer.
 
-	word = strtok(buffer, delims);
+	printf("Original string: %s\n", buffer);//printea el string que introdujo el usuario.
 
-	while (word != NULL) {
-		//printf("\nword: %s\n", word);
+	word = strtok(buffer, delims);//strtok divide el string en tokens. Hace una separacion cada vez que encuentra un simbolo de los que esta en "delims".
+
+	while (word != NULL) {//Este loop guarda cada token en un espacio dentro de "split". Repito que "split es un array de strings".
 		split[i] = word;
 		word = strtok(NULL, delims);
 		i++;
-		count++;
+		countTokens++;//Para contar los tokens
 	}
-	i++;
-	split[i] = '\0';
+	
 	i = 0;
-	while (i<count) {
+	while (i< countTokens) {//Esto es para probar que se guardaron bien los tokens.
 		printf("\n%s\n", split[i++]);			
 	}
-	free(split);
-	printf("count = %d", count);
 	
+	printf("Number of tokens = %d\n", countTokens);
+	split[countTokens] = "?";//Este signo de interrogacion lo puse al final del array para saber cuando parar. 
+	//Creo que se puede implementar de forma que no haga falta, pero lo voy a dejar asi por ahora.
+	countTokens++;
+
+
+	//----------------------------Rules---------------------------------
+	/*
+	Esto no esta perfecto porque VectorSum tiene que aceptar dos listas de numeros
+	y por ahora solo coge una.
+	*/
+	i = 1;//La variable i = 1 porque ya sabemos que el primer string es "VectorSum". Lo que le sigue tienen que ser numeros.
+	int countNumbers = 0;//Amount of numbers stored in array "numbers".
+	int numbers[50];
+	int equalStrings = strcmp(split[0], "VectorSum");//Si el primer token es VectorSum, guarda cero en la variable.
+	if (equalStrings == 0) {
+		while (strcmp(split[i],"?") != 0) {
+			numbers[i - 1] = atoi(split[i]);//Anade numeros a la lista y los convierte en enteros.
+			countNumbers++;//Cuenta cuantos numeros hay.
+			i++;
+		}
+	}
+	else
+		printf("Error");
+
+	i = 0;
+	printf("countNumbers = %d\n", countNumbers);
+	printf("List of numbers:\n");
+	for (i = 0; i < countNumbers; i++) {//Con esto pruebo si se guardan los numeros en el array "numbers".
+		printf("%d\n", numbers[i]);
+	}
 	
+	free(split);//Libera la memoria.
 	getchar();
 	return 0;
 }
+
+ 
+
+
 
