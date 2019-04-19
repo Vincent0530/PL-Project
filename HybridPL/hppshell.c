f87254b6f01123cd4fbc323237af58a7427ef84a
bernardo <bernardo@bernardo-HP-15-TS-Notebook-PC>
2019-04-19 14:32:26 -0400

    open mp testing

--- HybridPL/hppshell.c
+++ HybridPL/hppshell.c
@@ -1,37 +1,47 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
-#define lineSize 512
-#define bufferSize 1024
+#define BUFFERSIZE 1024
 
 const char* delims = " ,()";
 
+struct list {
+  int size;
+  int *listelements;
+};
+
 //definir funciones para los commands
-int vectorsum(char *funct);
+int vectorsum(char **funct);
+int vectorsub(char **funct);
+int vectormult(char **funct);
 
 //guarda los commands permitidos
 char *functs[] = {
-  "vectorsum"
+  "vectorsum","vectorsub","vectormult"
 };
 
 //siempre y cuando tengan el mismo indice, apunta a la direccion para ejecutar
 //el command dado
-int (*cmd_functs[]) (char *) = {
-  &vectorsum
+int (*cmd_functs[]) (char **) = {
+  &vectorsum, &vectorsub , &vectormult
 };
 
 //returns cuantos commands son permitidos
 int functcount(){ return sizeof(functs)/sizeof(char *); }
 
 //para implementar el Shell/Script
-char* readline();
-int dofunct(char *funct);
+char * readline();
+char ** linetok(char *funct);
+int dofunct(char **funct);
+struct list makelist(char **funct, int startindex);
 
 int main(int argc, char *argv[]){
   //A medida que se introducen commands, se guardaran en <line>
   char **line;
-  line = malloc(sizeof(char)*bufferSize*bufferSize);
+  line = malloc(sizeof(char)*BUFFERSIZE*BUFFERSIZE);
   int index = 0;
 
   while(1){
@@ -45,7 +55,10 @@ int main(int argc, char *argv[]){
     } else {
       //<dofunct()> busca la funcion a ejecutar para el command dado y devuelve
       //un numero diferente a 0 si la encuentra. Otherwise, devuelve 0.
-      if(dofunct(line[index]) != 0){
+      char **command;
+      command = malloc(sizeof(char)*BUFFERSIZE*BUFFERSIZE);
+      command = linetok(line[index]);
+      if(dofunct(command) != 0){
         printf("succesful execution\n");
       } else {
         printf("ERROR: command not found\n");
@@ -55,7 +68,7 @@ int main(int argc, char *argv[]){
 }
 
 char * readline(){
-  int c, pos = 0, bufSize = bufferSize;
+  int c, pos = 0, bufSize = BUFFERSIZE;
   char *read = malloc(sizeof(char)*bufSize);
   while(1){
     c = getchar();
@@ -68,15 +81,30 @@ char * readline(){
     pos++;
 
     if(pos >= bufSize){
-      bufSize += bufferSize;
+      bufSize += BUFFERSIZE;
       read = realloc(read, bufSize);
     }
   }
 }
 
-int dofunct(char *funct){
+char ** linetok(char *funct){
+  char *token;
+  char **tokens = (char **) malloc(sizeof(char*)*BUFFERSIZE);
+  int i = 0, counttok = 0;
+  token = strtok(funct, delims);
+  while(token != NULL){
+    tokens[i] = token;
+    token = strtok(NULL, delims);
+    i++;
+    counttok++;
+  }
+  tokens[counttok] = " ?"; //END OF TOKENS (como lo de Vincent)
+  return tokens;
+}
+
+int dofunct(char **funct){
   char *cmd;
-  cmd = strtok(cmd, delims);
+  cmd = funct[0];
 
   for(int i=0; i<functcount(); i++){
     if(strcmp(cmd, functs[i]) == 0){
@@ -88,9 +116,55 @@ int dofunct(char *funct){
   return 0;
 }
 
+struct list makelist(char **funct, int startindex){
+  struct list ltr;
+  ltr.listelements = malloc(sizeof(int)*BUFFERSIZE);
+  //Comenzar asignando elementos correspondientes a la lista
+  int cursor, listindex = 0;
+  cursor = atoi(strtok(funct[startindex], delims));
+  while(cursor != atoi("?") || cursor != atoi("|")){
+    ltr.listelements[listindex] = cursor;
+    listindex++;
+    startindex++;
+    cursor = atoi(strtok(funct[startindex], delims));
+  }
+  ltr.size = listindex;
+  return ltr;
+}
+
 //funcion para ejecutar vectorsum
 //Necesita implementacion
-int vectorsum(char *funct){
+int vectorsum(char **funct){
   printf("vectorsum!\n");
+  struct list list1 = makelist(funct, 1);
+  struct list list2 = makelist(funct, list1.size + 2);
+  printf("first element:%d, list 1 size:%d\n", list1.listelements[0], list1.size);
+  printf("first element:%d, list 2 size:%d\n", list2.listelements[0], list2.size);
+
+  //Main code for creating a thread for every element in the first list.
+  int result=0;
+  omp_set_num_threads(list1.size);
+#pragma omp parallel
+  {printf("%d \n",omp_get_thread_num());}
+
+
+  return 1;
+}
+//Template para sub
+int vectorsub(char **funct){
+  printf("vectorsub!\n");
+  struct list list1 = makelist(funct, 1);
+  struct list list2 = makelist(funct, list1.size + 2);
+  printf("first element:%d, list 1 size:%d\n", list1.listelements[0], list1.size);
+  printf("first element:%d, list 2 size:%d\n", list2.listelements[0], list2.size);
+  return 1;
+}
+//Template para mult
+int vectormult(char **funct){
+  printf("vectormult!\n");
+  struct list list1 = makelist(funct, 1);
+  struct list list2 = makelist(funct, list1.size + 2);
+  printf("first element:%d, list 1 size:%d\n", list1.listelements[0], list1.size);
+  printf("first element:%d, list 2 size:%d\n", list2.listelements[0], list2.size);
   return 1;
 }
--- HybridPL/main.c
+++ HybridPL/main.c
@@ -16,7 +16,7 @@ int main() {
 	int i = 0;//Esta variable es para iterar.
 	int countTokens = 0;//Esta variable cuenta el numero de tokens que se encuentran en "split".
 	const char * tokens[] = { "VECTORSUM","PIAPROX","RP","LP","COMMA" };//Esto no lo he usado para nada.
-	printf("Example:\nVectorSum(1 2 3 4 5 9 7 3 6 8)\npicalculation(10)\n\n");//Esto es un ejemplo del lenguage.
+	printf("Example:\nVectorSum 1 2 3 4 5 | 9 7 3 6 8\npicalculation(10)\n\n");//Esto es un ejemplo del lenguage.
 
 	gets(buffer, lineSize);//Esto lo que hace es que pide un string y lo guarda en el buffer.
 
@@ -48,32 +48,45 @@ int main() {
 	y por ahora solo coge una.
 	*/
 	i = 1;//La variable i = 1 porque ya sabemos que el primer string es "VectorSum". Lo que le sigue tienen que ser numeros.
-	int countNumbers = 0;//Amount of numbers stored in array "numbers".
-	int numbers[50];
+	int j = 0;
+	int listNumbers1[50];
+	int countNumbers1 = 0;//Amount of numbers stored in array "listNumbers1".
+	int listNumbers2[50];
+	int countNumbers2 = 0;//Amount of numbers stored in array "listNumbers2".
 	int equalStrings = strcmp(split[0], "VectorSum");//Si el primer token es VectorSum, guarda cero en la variable.
 	if (equalStrings == 0) {
-		while (strcmp(split[i],"?") != 0) {
-			numbers[i - 1] = atoi(split[i]);//Anade numeros a la lista y los convierte en enteros.
-			countNumbers++;//Cuenta cuantos numeros hay.
+		while (strcmp(split[i],"|") != 0) {//Interrogacion es el simbolo que puse al final para saber cuando parar de iterar.
+			//Aqui va un if por si acaso no se pone un "|".
+			listNumbers1[i - 1] = atoi(split[i]);//Anade numeros a la lista y los convierte en enteros.
+			countNumbers1++;//Cuenta cuantos numeros hay.
 			i++;
+		}
+
+		i++;
+		while (strcmp(split[i], "?") != 0) {
+			listNumbers2[j] = atoi(split[i]);
+			i++;
+			j++;
+			countNumbers2++;
 		}
 	}
 	else
 		printf("Error");
 
 	i = 0;
-	printf("countNumbers = %d\n", countNumbers);
-	printf("List of numbers:\n");
-	for (i = 0; i < countNumbers; i++) {//Con esto pruebo si se guardan los numeros en el array "numbers".
-		printf("%d\n", numbers[i]);
+	printf("countNumbers1 = %d\n", countNumbers1);
+	printf("List of numbers1:\n");
+	for (i = 0; i < countNumbers1; i++) {//Con esto pruebo si se guardan los numeros en el array "numbers".
+		printf("%d\n", listNumbers1[i]);
+	}
+
+	printf("countNumbers2 = %d\n", countNumbers2);
+	printf("List of numbers2:\n");
+	for (i = 0; i < countNumbers2; i++) {//Con esto pruebo si se guardan los numeros en el array "numbers".
+		printf("%d\n", listNumbers2[i]);
 	}
 	
 	free(split);//Libera la memoria.
 	getchar();
 	return 0;
 }
-
- 
-
-
-
