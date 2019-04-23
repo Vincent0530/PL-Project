1582c43791a1edbe55a10ccc37698bdd3105213d
ManuelCN <manuel.castaneda@upr.edu>
2019-04-22 23:54:06 -0400

    Added documentation and cleaned up code (revision 1)

--- HybridPL/hppshell.c
+++ HybridPL/hppshell.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 #define BUFFERSIZE 1024
-#pragma warning(disable : 4996)//No borren esto.
+#pragma warning(disable : 4996)//No borrar
 const char* delims = " ,()";
 int threadNum;
 struct list {
@@ -13,36 +13,38 @@ struct list {
 	double *listelements;
 };
 
-//definir funciones para los commands
+//Definir funciones de los differentes commands.
+
 int vectorsum(char **funct);
 int vectorsub(char **funct);
 int vectormult(char **funct);
 int vectordiv(char **funct);
-void setThreadNum(char **funct);
-void help();
+int setThreadNum(char **funct);
+int help();
 
-//guarda los commands permitidos
+//Arreglo para guardar el nombre de los diferentes commands currently supported.
 const char *functs[] = {
   "vectorsum","vectorsub","vectormult","vectordiv","help","setthreadnum"
 };
 
-//siempre y cuando tengan el mismo indice, apunta a la direccion para ejecutar
-//el command dado
+//Arreglo para guardar los addresses de las funciones de los diferentes commands currently supported.
 int(*cmd_functs[]) (char **) = {
   &vectorsum, &vectorsub , &vectormult , &vectordiv, &help, &setThreadNum
 };
 
-//returns cuantos commands son permitidos
+/**
+ * RETURN Cantidad de commands currently supported.
+*/
 int functcount() { return sizeof(functs) / sizeof(char *); }
 
-//para implementar el Shell/Script
+//Funciones para implementar el shell.
+
 char * readline();
 char ** linetok(char *funct);
 int dofunct(char **funct);
 struct list makelist(char **funct, int startindex);
 
 int main(int argc, char *argv[]) {
-	//A medida que se introducen commands, se guardaran en <line>
 	char **line;
 	line = (char**)malloc(sizeof(char)*BUFFERSIZE*BUFFERSIZE);
 	int index = 0;
@@ -50,29 +52,32 @@ int main(int argc, char *argv[]) {
 	while (1) {
 		printf("HPPShell>>> ");
 		line[index] = readline();
-		printf("Entered: %s\n", line[0]);
 		//El command exit cierra el shell
 		if (strcmp(line[index], "exit") == 0) {
-			printf("exit success\n");
+			printf("Programa terminado.\n");
 			exit(0);
 		}
 		else {
-			//<dofunct()> busca la funcion a ejecutar para el command dado y devuelve
-			//un numero diferente a 0 si la encuentra. Otherwise, devuelve 0.
 			char **command;
 			command = (char**)malloc(sizeof(char)*BUFFERSIZE*BUFFERSIZE);
 			command = linetok(line[index]);
-			if (dofunct(command) != 0) {
-				// printf("succesful execution\n");
-			}
-			else {
-				printf("ERROR: command not found\n");
+      int execute = dofunct(command);
+			if (execute == 0) {
+				//printf("SUCCESS\n");
+			} else if(execute == 1){
+        printf("ERROR: Listas de diferentes dimensiones/sizes.\n");
+      } else if(execute == 2) {
+				printf("ERROR: Command no encontrado o not currently supported.\n");
 			}
 		}
 	}
 }
 
 //------------------------lexer----------------------------------
+/**
+ * Se encarga de leer la linea introducida, caracter por caracter.
+ * RETURN El string introducido por el usuario.
+*/
 char * readline() {
   double c;
 	int pos = 0, bufSize = BUFFERSIZE;
@@ -94,7 +99,10 @@ char * readline() {
 		}
 	}
 }
-
+/**
+ * Se encarga de dividir en tokens el string de caracteres pasado como argumento.
+ * RETURN Arreglo de strings, en el que cada celda del arreglo es un token.
+*/
 char ** linetok(char *funct) {
 	char *token;
 	char **tokens = (char**)malloc(sizeof(char*)*BUFFERSIZE);
@@ -106,12 +114,16 @@ char ** linetok(char *funct) {
 		i++;
 		counttok++;
 	}
-	tokens[counttok] = "?"; //END OF TOKENS (como lo de Vincent)
+	tokens[counttok] = "?"; //END OF TOKENS
 	return tokens;
 }
 //-----------------------------------------------------------------
 
-
+/**
+ * Busca en el arreglo de commands currently supported. Si encuentra el command,
+ * ejecuta la funcion correspondiente.
+ * RETURN Resultado de la ejecucion de la funcion (status).
+*/
 int dofunct(char **funct) {
 	char *cmd;
 	cmd = funct[0];
@@ -121,11 +133,14 @@ int dofunct(char **funct) {
 			return (*cmd_functs[i])(funct);
 		}
 	}
-	//Si termina el loop sin return, significa que no encontro el command en los
-	//commands permitidos. Por lo tanto, devuelve 0.
-	return 0;
+	//Si termina sin return, command no esta currently supported.
+	return 2;
 }
 
+/**
+ * Crea una lista de los elementos introducidos por el usario.
+ * RETURN Estructura de lista para ejecutar commands.
+*/
 struct list makelist(char **funct, int startindex) {
 	struct list ltr;
 	ltr.listelements = malloc(sizeof(int)*BUFFERSIZE);
@@ -134,7 +149,6 @@ struct list makelist(char **funct, int startindex) {
   int listindex = 0;
 	cursor = strtod(strtok(funct[startindex], delims), NULL);
 	while (cursor != strtod("?", NULL) || cursor != strtod("|", NULL)) {
-    printf("cursor is: %0.2f\n", cursor);
 		ltr.listelements[listindex] = cursor;
 		listindex++;
 		startindex++;
@@ -143,176 +157,169 @@ struct list makelist(char **funct, int startindex) {
 	ltr.size = listindex;
 	return ltr;
 }
-
-void setThreadNum(char **funct) {
+/**
+ * Establece la cantidad de threads a usarse para la ejecucion de los commands en paralelo.
+*/
+int setThreadNum(char **funct) {
 	threadNum = atoi(funct[1]);
-	printf("\nthreadNum = %d\n", threadNum);
+	printf("Thread number set to: %d\n", threadNum);
+  return 0;
+}
+/**
+ * Imprime en el shell informacion general sobre el manejo del lenguaje.
+*/
+int help() {
+  printf("\n*****************HELP PAGE******************\n");
+  printf("Authors: Bernardo Jr. Sein, Coralys Cortes, Manuel Casta�eda, Vincent Prado\n");
+  printf("Es un lenguaje funcional para simplificar y facilitar la ejecucion en paralelo de commands.\n");
+  printf("Los vectores tienen que tener el mismo size, pero pueden ser de diferentes dimensiones.\n\n");
+  printf("Lista de commands currently supported:\n");
+  printf("* vectorsum double, double, ... | double, double, ... \tSuma de vectores\n");
+  printf("* vectorsub double, double, ... | double, double, ... \tResta de vectores\n");
+  printf("* vectormult double, double, ... | double, double, ... \tMultiplicacion de vectores\n");
+  printf("* vectordiv double, double, ... | double, double, ... \tDivision de vectores\n");
+  printf("\nEjemplo: vectorsum 1 2 3 4 | 1 2 3 4\n");
+  return 0;
 }
 
-void help() {
-	printf("\n\n *****************HELP PAGE******************\n");
-	printf("\n Authors:Bernardo Jr. Sein, Coralys Cortes, Manuel Casta�eda, Vincent Prado \n ");
-	printf("\n***** This is a hybrid language which automatically uses multiple threads for each function currently supported*****\n");
-	printf("Currently supported functions: \n");
-	printf("\n-vectorsum double double | double double       (sumation of vectors) \n"
-		"\n-vectorsub double double | double double      (substraction of vectors) \n"
-		"\n-vectormult double double | double double     (multiplication of vectors) \n"
-    "\n-vectordiv double double | double double      (division of vectors) \n"
-		"\n(vectors can have any amount of dimensions, but both vectors must be of equal sizes)\n \n"
-		"\n to exit the program type 'exit' \n\n");
-	printf("Syntax Example:\n\nvectorsum 1 2 3 4 5 | 6 7 8 9 10\n\n");
-}
-
-//funcion para ejecutar vectorsum
-//Necesita implementacion
+/**
+ * Suma los elementos correspondientes de 2 listas en paralelo usando una cantidad de threads determinada.
+ * RETURN Status la ejecucion del command.
+*/
 int vectorsum(char **funct) {
-	printf("vectorsum!\n");
-	struct list list1 = makelist(funct, 1);//El segundo parametro es igual a 1 porque el primera string de la lista es el comando que define la operacion
+	struct list list1 = makelist(funct, 1);
 	struct list list2 = makelist(funct, list1.size + 2);
-	printf("first element:%0.2f, list 1 size:%d\n", list1.listelements[0], list1.size);
-	printf("first element:%0.2f, list 2 size:%d\n", list2.listelements[0], list2.size);
 	int n = list1.size;
-
-
 
 	struct list result;
 	result.listelements = malloc(sizeof(int)*BUFFERSIZE);
 
 	if (list1.size == list2.size) {
-		//clock_t t;
-		//t = clock();
 		int chunck = 5;
-		printf("\nthreadNum = %d\n", threadNum);
-		printf("\nchunck = %d\n", chunck);
+		printf("Thread count: %d\n", threadNum);
+		printf("Chunk: %d\n", chunck);
 		int i;
 		omp_set_num_threads(threadNum);
-#pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
-		for (i = 0; i < n; i++) {
-			result.listelements[i] = list1.listelements[i] + list2.listelements[i];
-			printf("threadID = %d\n", omp_get_thread_num());
-		}
-
-
-		printf("\nresult: ");
+    #pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
+      for (i = 0; i < n; i++) {
+        result.listelements[i] = list1.listelements[i] + list2.listelements[i];
+        //printf("threadID = %d\n", omp_get_thread_num());
+      }
+		printf("Result:\t");
 		for (int i = 0; i < n; i++) {
-			printf(" %0.2f ", result.listelements[i]);
+			printf("%0.2f ", result.listelements[i]);
 		}
 		printf("\n");
-		//t = clock() - t;
-		//double time_taken = ((double)t) / CLOCKS_PER_SEC;
-	   // printf("Time taken = %f\n", time_taken);
-
+    return 0;
+	} else {
+		return 1;
 	}
-	else {
-		printf("\nError: Vectors Must be of equal size. \n");
-	}
-
-
-
-	return 1;
 }
-//Template para sub
+
+/**
+ * Resta los elementos correspondientes de 2 listas en paralelo usando una cantidad de threads determinada.
+ * RETURN Status de la ejecucion del command.
+*/
 int vectorsub(char **funct) {
-	printf("vectorsub!\n");
 	struct list list1 = makelist(funct, 1);
 	struct list list2 = makelist(funct, list1.size + 2);
-	printf("first element:%0.2f, list 1 size:%d\n", list1.listelements[0], list1.size);
-	printf("first element:%0.2f, list 2 size:%d\n", list2.listelements[0], list2.size);
 	int n = list1.size;
+
 	struct list result;
 	result.listelements = malloc(sizeof(int)*BUFFERSIZE);
+
 	if (list1.size == list2.size) {
 		int chunck = 5;
-		printf("\nthreadNum = %d\n", threadNum);
-		printf("\nchunck = %d\n", chunck);
+		printf("Thread count: %d\n", threadNum);
+		printf("Chunk: %d\n", chunck);
 		int i;
 		omp_set_num_threads(threadNum);
-#pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
-		for (i = 0; i < n; i++) {
-			result.listelements[i] = list1.listelements[i] - list2.listelements[i];
-			printf("threadID = %d\n", omp_get_thread_num());
-		}
+    #pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
+      for (i = 0; i < n; i++) {
+        result.listelements[i] = list1.listelements[i] - list2.listelements[i];
+        //printf("threadID = %d\n", omp_get_thread_num());
+      }
 
-		printf("\nresult: ");
+		printf("Result:\t");
 		for (int i = 0; i < list1.size; i++) {
-			printf(" %0.2f ", result.listelements[i]);
+			printf("%0.2f ", result.listelements[i]);
 		}
 		printf("\n");
+    return 0;
 	}
 	else {
-		printf("\nError: Vectors Must be of equal size. \n");
-
+		return 1;
 	}
-
-	return 1;
 }
-//Template para mult
+
+/**
+ * Multiplica los elementos correspondientes de 2 listas en paralelo usando una cantidad de threads determinada.
+ * RETURN Status de la ejecucion del command.
+*/
 int vectormult(char **funct) {
-	printf("vectormult!\n");
 	struct list list1 = makelist(funct, 1);
 	struct list list2 = makelist(funct, list1.size + 2);
-	printf("first element:%0.2f, list 1 size:%d\n", list1.listelements[0], list1.size);
-	printf("first element:%0.2f, list 2 size:%d\n", list2.listelements[0], list2.size);
 	int n = list1.size;
+
 	struct list result;
 	result.listelements = malloc(sizeof(int)*BUFFERSIZE);
+
 	if (list1.size == list2.size) {
 		int chunck = 5;
-		printf("\nthreadNum = %d\n", threadNum);
-		printf("\nchunck = %d\n", chunck);
+		printf("Thread count: %d\n", threadNum);
+		printf("Chunk: %d\n", chunck);
 		int i;
 		omp_set_num_threads(threadNum);
 		#pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
 		for (i = 0; i < n; i++) {
 			result.listelements[i] = list1.listelements[i] * list2.listelements[i];
-			printf("threadID = %d\n", omp_get_thread_num());
+			//printf("threadID = %d\n", omp_get_thread_num());
 		}
 
-		printf("\nresult: ");
+		printf("Result:\t");
 		for (int i = 0; i < list1.size; i++) {
-			printf(" %0.2f ", result.listelements[i]);
+			printf("%0.2f ", result.listelements[i]);
 		}
 		printf("\n");
+    return 0;
 	}
 	else {
-		printf("\nError: Vectors Must be of equal size. \n");
-
+		return 1;
 	}
-
-	return 1;
 }
-//Template para div
+
+/**
+ * Divide los elementos correspondientes de 2 listas en paralelo usando una cantidad de threads determinada.
+ * RETURN Status de la ejecucion del command.
+*/
 int vectordiv(char **funct) {
-	printf("vectordiv!\n");
 	struct list list1 = makelist(funct, 1);
 	struct list list2 = makelist(funct, list1.size + 2);
-	printf("first element:%0.2f, list 1 size:%d\n", list1.listelements[0], list1.size);
-	printf("first element:%0.2f, list 2 size:%d\n", list2.listelements[0], list2.size);
 	int n = list1.size;
+
 	struct list result;
 	result.listelements = malloc(sizeof(int)*BUFFERSIZE);
+
 	if (list1.size == list2.size) {
 		int chunck = 5;
-		printf("\nthreadNum = %d\n", threadNum);
-		printf("\nchunck = %d\n", chunck);
+		printf("Thread count: %d\n", threadNum);
+		printf("Chunk: %d\n", chunck);
 		int i;
 		omp_set_num_threads(threadNum);
 		#pragma omp parallel for shared (list1,list2,result,n) private(i) schedule(static,chunck)
 		for (i = 0; i < n; i++) {
 			result.listelements[i] = list1.listelements[i] / list2.listelements[i];
-			printf("threadID = %d\n", omp_get_thread_num());
+			//printf("threadID = %d\n", omp_get_thread_num());
 		}
 
-		printf("\nresult: ");
+		printf("Result:\t");
 		for (int i = 0; i < list1.size; i++) {
-			printf(" %0.2f ", result.listelements[i]);
+			printf("%0.2f ", result.listelements[i]);
 		}
 		printf("\n");
+    return 0;
 	}
 	else {
-		printf("\nError: Vectors Must be of equal size. \n");
-
+		return 1;
 	}
-
-	return 1;
 }
